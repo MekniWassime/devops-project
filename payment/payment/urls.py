c61050728b2a55573adcecf72a268b8286327d80
Mekni_Wassime <60438665+MekniWassime@users.noreply.github.com>
2023-01-17 15:21:27 +0100

    update payment app

--- payment/payment/urls.py
+++ payment/payment/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path
+from django.urls import path, include
 from payment_app.views import PaymentList
 
 urlpatterns = [
